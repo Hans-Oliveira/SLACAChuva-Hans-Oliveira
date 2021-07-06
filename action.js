0153651cae2f1733292bc4b2fa1404ef7cd014d4
Matheus Hans Duffeck de Oliveira <hansduffeck@gmail.com>
2021-07-06 16:17:37 -0300

    Interação
    Interação de abertura do tópico que tem respostas.
    implementação  aa mensagem após a submissão do formulário.

--- action.js
+++ action.js
@@ -47,7 +47,10 @@ function creatDivTopic (){
     let allTopics = document.getElementById("allTopics");
     let textArea = document.getElementById("textArea").value;
     let topic = document.getElementById("topic").value;
+    let divdaddy = document.createElement("div");
+    divdaddy.className = "divDaddy"
     let div = document.createElement("div");
+    div.style.opacity = "0.2"
     div.className = "question";
     let h3 = document.createElement("h3");
     let p = document.createElement("p");
@@ -63,16 +66,38 @@ function creatDivTopic (){
     input2.type = "submit";
     input2.value = "";
     let h6 = document.createElement("h6");
-    let h62 = document.createElement("h6");
+    let h62 = document.createElement("h6"); 
+    let img = document.createElement("img");
+    img.src = "image/sucesse.svg"
+    let pDaddy = document.createElement("p");
+    let aDaddy = document.createElement("a");
+    let writheA = document.createTextNode('Editar tópico');
+    let section1 = document.createElement("section");
+    section1.className = "section-daddy";
+    let section2 = document.createElement("section");
+    section2.className = "section-daddy";
+    let section3 = document.createElement("section");
+    section3.className = "section-daddy";
+    section3.style.marginBottom = "-5%"
+
 
     h33 = document.createTextNode(topic);
     pp = document.createTextNode('Nome da pessoa, logada no google!'); /* aqui receberia o nome do usuario logado pelo google com comando BasicProfile.getName () */
     pree = document.createTextNode(textArea);
     h66 = document.createTextNode('1 like');
     h622 = document.createTextNode('1 resposta');
+    pDaddy = document.createTextNode('Aguardando feedback dos autores');
+    
 
-
-
+    divdaddy.appendChild(section1);
+    section1.appendChild(img);
+    divdaddy.appendChild(section2);
+    section2.appendChild(pDaddy);
+    divdaddy.appendChild(section3);
+    section3.appendChild(aDaddy);
+    aDaddy.appendChild(writheA);
+    
+    divdaddy.appendChild(div);
     div.appendChild(h3);
     h3.appendChild(h33);
     div.appendChild(p);
@@ -89,15 +114,30 @@ function creatDivTopic (){
     div2.appendChild(h62);
     h62.appendChild(h622);
     
-    allTopics.appendChild(div);
+    allTopics.appendChild(divdaddy);
 
     h3 = document.createTextNode('Assunto da Pergunta aparece aqui');
     p = document.createTextNode('Nome da pessoa!');
     pre = document.createTextNode('Comecinho da pergunta aparece aqui resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo...');
     h6 = document.createTextNode('1 like');
     h62 = document.createTextNode('1 resposta');
-}
 
+    let sucesse = document.getElementById("sucesse");
+    sucesse.style.display = "block"
+    let createTopic = document.getElementById("createTopic");
+    createTopic.style.display = "none"
+    
+}
+function newTopic () {
+    let createTopic = document.getElementById("createTopic");
+    createTopic.style.display = "block"
+    let sucesse = document.getElementById("sucesse");
+    sucesse.style.display = "none"
+    let textArea = document.getElementById("textArea").value;
+    let topic = document.getElementById("topic").value;
+    textArea.value = "";
+    topic.value = "";
+}
 
 
 
